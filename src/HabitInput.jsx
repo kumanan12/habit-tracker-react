76a6fb690bba267b1e8eac8f89dbcfae4f6847ad
Krrithik <krrithikezhil30@gmail.com>
2025-02-12 21:20:24 -0800

    stubbed function handlers on both input and button

--- src/HabitInput.jsx
+++ src/HabitInput.jsx
@@ -1,10 +1,22 @@
 export default function HabitInput() {
+
+    function onInputChangeHandler(event){
+        let newHabit = event.target.value;
+        console.log(newHabit);
+        
+        
+    }
+
+    function onAddHandler(){
+        console.log('add button clicked'); 
+    }
+
   return (
     <>
       <div id="habit-input-box">
         <label htmlFor="habit-input">Habit: </label>
-        <input type="text" id="habit-input" name="habit-input" />
-        <button id="add-habit">Add</button>
+        <input type="text" id="habit-input" name="habit-input" placeholder="Add a habit..." onChange={onInputChangeHandler}/>
+        <button id="add-habit" onClick={onAddHandler}>Add</button>
         <button id="save-habit">Save</button>
         <button id="Load-habit">Load</button>
       </div>
